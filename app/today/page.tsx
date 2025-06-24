d1dd119c2bc8fe72683ca3d357a537e933dca6b4
Kashyap Patel <kashyap.p@simformsolutions.com>
2025-06-24 18:08:06 +0530

    Add Today component with calendar and task management functionality

---
+++ app/today/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import AddTask from "../components/AddTask";
+import { CalendarPage } from "../components/CalenderPage";
+
+export default function Today() {
+  const [showTaskForm, setShowTaskForm] = useState(false);
+
+  return (
+    <main className="bg-gray-50 max-h-screen">
+      <div className="flex bg-white h-full">
+        {/* Calendar Section - Scrollable */}
+        <section className="w-full h-full">
+          <CalendarPage
+            showTaskForm={showTaskForm}
+            toggleTaskForm={setShowTaskForm}
+          />
+        </section>
+        {/* Add Task Section - Sticky */}
+        {showTaskForm && (
+          <aside className="w-1/4 border-l-2 border-gray-200 bg-indigo-100">
+            <div className="sticky top-0 ">
+              <AddTask />
+            </div>
+          </aside>
+        )}
+      </div>
+    </main>
+  );
+}
